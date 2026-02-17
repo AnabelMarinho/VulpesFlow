ee2a8afa311d37438449a5a0f009a5cab91a134e
AnabelMarinho <anabel.soares2001@gmail.com>
2026-02-17 13:39:48 -0300

    feat: criação da seção de projetos

--- src/app/homeSections/projects/projects.ts
+++ src/app/homeSections/projects/projects.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SectionHeader } from '../../components/section-header/section-header';
+import { Button } from '../../components/button/button';
+import { Card } from '../../components/card/card';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [SectionHeader, Button, Card],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -5,11 +5,12 @@ import { Header } from '../../components/header/header';
 import { Services } from '../../homeSections/services/services';
 import { Equipe } from '../../homeSections/equipe/equipe';
 import { About } from '../../homeSections/about/about';
+import { Projects } from '../../homeSections/projects/projects';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Button, Footer, Header, About, Services, Equipe],
+  imports: [Button, Footer, Header, About, Services, Projects, Equipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
